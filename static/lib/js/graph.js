36fb6ca0f49316b9f0afd2a9f7c86a10afa87ff7
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-06-13 21:03:14 +0100

    Added Crossfilter instance and set up data Dimensions

---
+++ static/lib/js/graph.js
@@ -0,0 +1,38 @@
+queue()
+    .defer(d3.json, "/nasaData/missions")
+    .await(makeGraphs);
+
+function makeGraphs(error, missionsJson) {
+    var nasaDataMissions = missionsJson;
+    var dateFormat = d3.time.format("%Y-%m-%d");
+    nasaDataMissions.forEach(function(d) {
+       d["Date"] = dateFormat.parse(d["Date"]);
+       d["Date"].setDate(1);
+       d["Country"] = +d["Country"];
+    });
+
+    // create a Crossfilter instance
+    var ndx = crossfilter(nasaDataMissions);
+
+    // Define data Dimensions
+    var countryDim = ndx.dimension(function(d) {
+        return d["Country"]; });
+
+    var dateDim = ndx.dimension(function(d) {
+        return d["Date"]; });
+
+    var vehicleDim = ndx.dimension(function(d) {
+        return d["Vehicle"]; });
+
+    var durationDim = ndx.dimension(function(d) {
+        return d["Duration"]; });
+
+    var purposeDim = ndx.dimension(function(d) {
+        return d["Purpose"]; });
+
+    var crewDim = ndx.dimension(function(d) {
+        return d["Crew"]; });
+
+    // Define data groups
+    var all = ndx.groupAll();
+}
