2e773a6bc08d38d2bc7de67517a1c62d87ff8668
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-25 21:50:42 +0100

    undo fix

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,14 +1,7 @@
 // script to control the modal information window
 // that pops up on page load
 
-$(document).ready(function(){
-    setTimeout(function(){
-    if(!Cookies.get('modalShown')) {
-        $("#myModal").modal('show');
-      Cookies.set('modalShown', true);
-    } else {
-        alert('Your modal won\'t show again as it\'s shown before.');
-    }
-
-},3000);
+$(window).load(function()
+{
+    $('#myModal').modal('show');
 });
