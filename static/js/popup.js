1422def91a3fac5c1f242910ab30e91c681fef27
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-25 21:48:53 +0100

    attempted fix for modal loading only once

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,7 +1,14 @@
 // script to control the modal information window
 // that pops up on page load
 
-$(window).load(function()
-{
-    $('#myModal').modal('show');
+$(document).ready(function(){
+    setTimeout(function(){
+    if(!Cookies.get('modalShown')) {
+        $("#myModal").modal('show');
+      Cookies.set('modalShown', true);
+    } else {
+        alert('Your modal won\'t show again as it\'s shown before.');
+    }
+
+},3000);
 });
