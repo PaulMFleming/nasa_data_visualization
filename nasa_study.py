996b1ff1294980ea536758c1c56d3b80b6112318
Paul Fleming <paul.stillbreathing@gmail.com>
2024-04-01 16:07:00 +0100

    switch from MongoDB to Postgres

--- nasa_study.py
+++ nasa_study.py
@@ -1,10 +1,34 @@
 from flask import Flask
 from flask import render_template
-from pymongo import MongoClient
+from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Mission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    eva_number = db.Column(db.Integer)
+    country = db.Column(db.String(255))
+    crew = db.Column(db.String(255))
+    vehicle = db.Column(db.String(255))
+    date = db.Column(db.String(255))
+    duration = db.Column(db.String(255))
+    purpose = db.Column(db.Text)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'eva_number': self.eva_number,
+            'country': self.country,
+            'crew': self.crew,
+            'vehicle': self.vehicle,
+            'date': self.date,
+            'duration': self.duration,
+            'purpose': self.purpose
+        }
 
 MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
 DBS_NAME = os.getenv('MONGO_DB_NAME', 'nasaData')
@@ -24,26 +48,13 @@ def videos():
 def nasa_missions():
     """
     A Flask view to serve the project data
-    from MongoDB in JSON format.
+    from PostgreSQL in JSON format.
     """
-
-    # A constant that defines the record fields we wish to retrieve.
-    FIELDS = {
-        '_id' : False, 'EVA #' : True, 'Country' : True,
-        'Crew' : True, 'Vehicle' : True, 'Model #' : True, 'Date' : True,
-        'Duration' : True, 'Purpose' : True
-    }
-
-    # Open a connection to MongoDB using a with statement
-    # connection closes as soon as we exit the with statement
-    with MongoClient(MONGO_URI) as conn:
-        # Define collection to access
-        collection = conn[DBS_NAME][COLLECTION_NAME]
-        # Retrieve results based on FIELDS and set a limit of results
-        projects = collection.find(projection=FIELDS, limit=10000)
-        # Convert to JSON and return the JSON data
-        return json.dumps(list(projects))
-
+    missions = db.session.query(Mission).all()
+    missions_json = [mission.to_dict() for mission in missions]
+    return json.dumps(missions_json)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
